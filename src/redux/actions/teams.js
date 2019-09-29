32ddb431b000e58e3045c287fdc7b46967cc0742
Whitny Edwards <wne4e2@gmail.com>
2019-09-29 15:55:30 -0400

    add fetch request to fetch all teams and add fetch request to create new team

--- src/redux/actions/teams.js
+++ src/redux/actions/teams.js
@@ -1,8 +1,35 @@
+const fetchTeams = (teams) => {
+    return {
+        type: 'FETCH_TEAMS_SUCCESS', 
+        teams  
+    }
+}
+
 export const getTeams = () => {
     return function(dispatch) {
-
     return fetch(`http://localhost:3001/teams`)
         .then(res => res.json())
-        .then(teams => dispatch({type: 'FETCH_TEAMS_SUCCESS', payload: teams}))
+        .then(teams => dispatch(fetchTeams(teams)))
     };
 };
+
+export const createTeam = (teamObject) => {
+    const teamToCreate = { team: teamObject };
+    return dispatch => {
+            fetch(`http://localhost:3001/teams`, {
+                method: 'POST',
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({teamToCreate})
+            })
+            .then(res => res.json())
+            .then(team => 
+                dispatch({
+                    type: 'TEAM_CREATE_SUCCESS', 
+                    payload: team
+                })
+            )
+        }
+}
