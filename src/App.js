d7572c222ee9ed24c8f846c7d255f31b803a4fd6
Whitny Edwards <wne4e2@gmail.com>
2019-09-29 15:53:21 -0400

    update to import getTeams and add componentDidMount

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home';
 import TeamsList from './containers/TeamsList';
+import NewTeam from './components/NewTeam'
+import { connect } from 'react-redux'
+import { getTeams } from './redux/actions/teams'
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/teams" component={TeamsList} />
-      </Router>
+class App extends React.Component {
+  componentDidMount() {
+    this.props.getTeams();
 
-    </div>
-  );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/teams" component={TeamsList} />
+          <Route exact path="/teams/new" component={NewTeam} />
+        </Router>
+      </div>
+    )
+  };
 }
 
-export default App;
+export default connect(
+  null, 
+    { getTeams }
+  )(App);
