092ec85badad2ce77454a393e49e2b61875c62ac
Whitny Edwards <wne4e2@gmail.com>
2019-09-28 21:11:57 -0400

    add thunk

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import teamsReducer from './reducers/teams'
+import thunk from 'redux-thunk'
 
 let store = createStore(
     teamsReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
- );
+    applyMiddleware(thunk)
+);
 
  export default store; 
