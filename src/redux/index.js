f8e032391cdf1acd53ae62cfc307263824c186eb
Whitny Edwards <wne4e2@gmail.com>
2019-09-28 22:22:33 -0400

    import compose and update syntax

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,9 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import teamsReducer from './reducers/teams'
 import thunk from 'redux-thunk'
 
-let store = createStore(
-    teamsReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(teamsReducer, composeEnhancer(applyMiddleware(thunk)));
 
  export default store; 
